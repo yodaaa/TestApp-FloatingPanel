e070d0e256d91aa7a2f13644830e5473d7b358dd
yodaaa <01ewell0@gmail.com>
2019-04-16 14:05:55 +0900

    Initial Commit

---
+++ TestApp-FloatingPanel/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestApp-FloatingPanel
+//
+//  Created by yodaaa on 2019/04/16.
+//  Copyright © 2019 yodaaa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
